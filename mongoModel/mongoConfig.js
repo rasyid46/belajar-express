e4c344e17e7514b41f5d4a76e4a321200cebd9e0
Sulaeman <sulaeman@rasyid.com>
2019-10-27 13:45:54 +0700

    mongoCOnfig

--- mongoModel/mongoConfig.js
+++ mongoModel/mongoConfig.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI
+// const MONGODB_URI = process.env.MONGODB_URI
 // Mongoose.connect(MONGODB_URI);
-Mongoose.connect("mongodb://semangka:17agustus@ds241097.mlab.com:39858/heroku_4p38v2k5?retryWrites=false");
+Mongoose.connect("mongodb://semangka:17agustus@ds239858.mlab.com:39858/heroku_4p38v2k5?retryWrites=false");
 module.exports = Mongoose;
-
+ 
