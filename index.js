e3575d02476c76beadcd25b61c2fd449f9b5a1ec
sulaeman rasyid <sulaeman.rasyid@emveep.com>
2020-11-09 22:45:33 +0700

    JWT decode apple

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express') 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,25 +1,64 @@
 var express = require('express');
 var router = express.Router(); 
+const jwksClient = require('jwks-rsa');
 
-const Mongoose = require('../mongoModel/mongoConfig')
-const PersonModel = Mongoose.model("person", {
-    firstname: String,
-    lastname: String
-}); 
+const jwt = require('jsonwebtoken');
+ 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('inie');
 });
+const client = jwksClient({
+  
+  jwksUri: 'https://appleid.apple.com/auth/keys',
+   
+});
+function getAppleSignKey(kid){
+  return new Promise((resolve)=>{
+    client.getSigningKey(kid, (err, key) => {
+      if(err){
+        console.error(error)
+        resolve(null)
+      }
+      const signingKey = key.getPublicKey(); 
+       resolve(signingKey)
+    })
+  })
+}
 
+function verifyJWT(json, publicKey){
+  return  new Promise((resolve) =>{
+    jwt.verify(json,publicKey, (err,payload) =>{
+      if(err){
+        console.log('errrrr verifyJWT')
+        return resolve(null)
+      }
+      resolve(payload)
+    })
+
+  })
+}
 router.get('/list', async (req, res) => {
   // Do something here
-  var person = await PersonModel.find().exec();
-  const response = {
+   let id_apple="eyJraWQiOiJlWGF1bm1MIiwiYWxnIjoiUlMyNTYifQ.eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLmZpbmFrdSIsImV4cCI6MTYwNTAwMDE2MiwiaWF0IjoxNjA0OTEzNzYyLCJzdWIiOiIwMDAxMTQuMTQ4NGQ2OTM2ODhmNGVkMzg0OWRjOWJiNDZhODIxM2YuMDQ0MCIsIm5vbmNlIjoiYzFhMGM1MzdiYzU0YTZlZDZiYzIzMTE3YzJhNGJhYjAwZDRiNjk0OGFiMzU3Y2FjNzMwOThhZWFmMzgxYjI3NyIsImNfaGFzaCI6InptVHRTWjI0eHRidmY0MEJmcE5IUUEiLCJlbWFpbCI6ImRpbWFzaHB0QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjoidHJ1ZSIsImF1dGhfdGltZSI6MTYwNDkxMzc2Miwibm9uY2Vfc3VwcG9ydGVkIjp0cnVlfQ.NpsDCtrP_FwSHyVafn5m5A2IcKGeWtyx43zbKq1VlDnsqhd5F9I92rj-X1-NoRKE4BWl6vBFkwbFUM576ZVDLn8fP3tPAnMACEocft7sNADsOL6Nhl1TLv5q2UpDhJ4w8hFHReNJFzSyfakpLKlVWtX6nN8mYk6Ao7XTg0Yairb7gNpfb60GndTXgpxpS77_b7s_rykWmQyTv4j6z3X1mDZJslBGu7bWpCpOJ-MmQe_o0VqN5j_zAFPhOFzF6uyCdoPW0Blr4LLCwJ7aFyslSFB4Ct82imU34KPWxaw9L3qckfZLlpcFnkhxLwunA_Khh0JSDwpIdtwb_3XgLwihBQ";
+   const json = jwt.decode(id_apple, {complete: true});
+
+   const kid = json.header.kid
+   const appleKeys = await getAppleSignKey(kid);
+   
+  if(!appleKeys){
+    console.error('Error');
+    return
+  }
+  const payload=  await verifyJWT(id_apple,appleKeys)
+
+   const response = {
       statusCode : 200,
-      error : "",
-      message : "List Person", 
-      content : person
+      kid : kid,
+      appleKeys : appleKeys, 
+      json : json,
+      payload:payload
   } 
   res.json(response);
 })
